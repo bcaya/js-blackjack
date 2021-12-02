d7b8afbeb13b27c64363b766b981395f4c9e07c0
Bobby Caya <bob.caya1@gmail.com>
2021-12-01 21:29:58 -0700

    added start game button with startgame function

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ let hasBlackjack = false
 let isAlive = true
 let message = "" //initialize blank message as placeholder 
 
-if(sum <= 20){
-  message = "Do you want a new card?"
-}else if(sum === 21){
-  message = "Woohoo! You got Blackjack!"
-  hasBlackjack = true 
-}else{
-  message = "You're out of the game!"
-  isAlive = false
+function startGame(){
+  if(sum <= 20){
+    message = "Do you want a new card?"
+  }else if(sum === 21){
+    message = "Woohoo! You got Blackjack!"
+    hasBlackjack = true 
+  }else{
+    message = "You're out of the game!"
+    isAlive = false
+  }
+
+  console.log(message)
 }
 
-console.log(message)
