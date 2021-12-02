f4d6fb07a80a4635c09279fb0e80fee1d1a3791f
Bobby Caya <bob.caya1@gmail.com>
2021-12-01 22:40:41 -0700

    display sum, cards and initial message

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ let sum = firstCard + secondCard + 13
 let hasBlackjack = false
 let isAlive = true
 let message = "" //initialize blank message as placeholder 
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard 
+  sumEl.textContent = "Sum is: " + sum
   if(sum <= 20){
     message = "Do you want a new card?"
   }else if(sum === 21){
-    message = "Woohoo! You got Blackjack!"
+    message = "You got Blackjack!"
     hasBlackjack = true 
   }else{
     message = "You're out of the game!"
     isAlive = false
   }
 
-  console.log(message)
+  messageEl.textContent = message 
 }
 
