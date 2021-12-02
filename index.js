c9bdd29f42e59a977673b1ef0a4e33feff2e0735
Bobby Caya <bob.caya1@gmail.com>
2021-12-01 20:16:52 -0700

    added functions to check if card values were 21 or not

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-let firstCard = 4 
-let secondCard = 5 
+let firstCard = 11 
+let secondCard = 10 
+let sum = firstCard + secondCard   
 
-let sum = firstCard + secondCard 
+if(sum <= 20){
+  console.log("Do you want a new card?")
+}else if(sum === 21){
+  console.log("Woohoo! You got Blackjack!")
+}else{
+console.log("You're out of the game!")
+}
