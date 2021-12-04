f04a16263d524b4854c8c708db699120f44730aa
Bobby Caya <bob.caya1@gmail.com>
2021-12-03 20:19:10 -0700

    added generate new card function

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-let firstCard = 5 
-let secondCard = 6 
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard   
+
+let cards = []
+let sum = 0  
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false 
 let message = "" //initialize blank message as placeholder 
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+console.log(cards)
+
+function getRandomCard(){
+  let randomCard = Math.floor(Math.random()* 13) + 1
+  if( randomCard >= 11){
+    return 10
+  }else if(randomCard === 1){
+    return 11
+  }else{
+    return randomCard
+  }
+  
+}
+
 function startGame(){
+  isAlive = true
+  let firstCard = getRandomCard() 
+  let secondCard = getRandomCard()
+  sum = firstCard + secondCard
+  cards.push(firstCard, secondCard)
+
   renderGame()
 }
 
 function renderGame(){
-  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1] 
+  cardsEl.textContent = "Cards: "
+  for(let i = 0; i < cards.length; i++){
+    cardsEl.textContent += cards[i] + " "
+  } 
   sumEl.textContent = "Sum is: " + sum
   if(sum <= 20){
     message = "Do you want a new card?"
@@ -30,8 +52,9 @@ function renderGame(){
 }
 
 function newCard(){
-  let card = 7
+  let card = getRandomCard()
   sum += card 
   cards.push(card)
+  console.log(cards)
   renderGame()
 }
